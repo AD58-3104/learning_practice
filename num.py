0bd904cd900af7a75ade9dd30db745b6150f8861
AD58-3104 <inoue.kunkun3104@gmail.com>
2025-05-19 02:41:11 +0900

    a

--- num.py
+++ num.py
@@ -1,13 +1,6 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import torch
 
 
-
-ar = np.zeros((4,4,4),dtype=float)
-ar[2][3] = 10
-print(ar)
-
-index = np.argmax(ar)
-
-print(np.unravel_index(index,ar.shape))
-print(ar[(2,3)])
+ten = torch.tensor([1, 2, 3, 4, 5]).unsqueeze(0)
+print(ten)
+print(ten.shape)
