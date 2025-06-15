730c3daf6ed9ef019436663d4fc09d75bc8e548d
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2025-06-16 04:05:36 +0900

    change

--- humanoidprac/humanoidprac/scripts/skrl/generate_config_list.py
+++ humanoidprac/humanoidprac/scripts/skrl/generate_config_list.py
@@ -17,12 +17,13 @@ def generate_combinations_as_lists(item_list):
     return combinations_list
 
 
-def generate_joint_strings(string_list, float_val:float):
+def generate_joint_strings(index:int,string_list, float_val:float):
     """
     文字列のリストと浮動小数点のリストから、
     0を浮動小数点の値に、"right_hip_roll"を文字列の値に置き換えた文字列を生成する
     
     Args:
+        index:int  インデックス(外から与える)
         string_list: 文字列のリスト
         float_list: 浮動小数点のリスト
     
@@ -36,10 +37,13 @@ def generate_joint_strings(string_list, float_val:float):
     #     for float_val in float_list:
             # テンプレート文字列で0とright_hip_rollを置き換え
     template = """  - joint_torque: [0]
-    joint_names: ["right_hip_roll"]"""
+    joint_names: ["right_hip_roll"]
+    index: num"""
         
         # 置き換えを実行
-    new_string = template.replace("0", ','.join(map(str,[float_val] * len(string_list)))).replace('"right_hip_roll"', ','.join(map(lambda s: f'"{s}"',string_list)))
+    template = template.replace("0", ','.join(map(str,[float_val] * len(string_list))))
+    template = template.replace('"right_hip_roll"', ','.join(map(lambda s: f'"{s}"',string_list)))
+    new_string = template.replace("num",str(index))
     result_strings.append(new_string)
     
     return result_strings
@@ -67,10 +71,9 @@ if __name__ == "__main__":
     for num in float_elem_list:
         for names in string_elem_list:
             # 文字列生成
-            generated_strings = generate_joint_strings([names], num)
-            
+            generated_strings = generate_joint_strings(iterate_num + 1,[names], num)
+            iterate_num += 1
             # 結果を表示
             for i, generated_string in enumerate(generated_strings, 1):
                 print(generated_string)
-                iterate_num += 1
     print(f"Iterate {iterate_num} times...")
