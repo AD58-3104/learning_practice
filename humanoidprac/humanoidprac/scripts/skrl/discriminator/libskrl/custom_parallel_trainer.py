ffddfaa91f9a7f9245f4637a582ee51667cc2e02
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2025-11-28 04:28:26 +0900

    複数モデルの切り替え学習のwip

---
+++ humanoidprac/humanoidprac/scripts/skrl/discriminator/libskrl/custom_parallel_trainer.py
@@ -0,0 +1,330 @@
+from typing import List, Optional, Union
+import queue
+
+import copy
+import sys
+import tqdm
+
+import torch
+
+from skrl.agents.torch import Agent
+from skrl.envs.wrappers.torch import Wrapper
+from skrl.trainers.torch import Trainer
+
+class StateHistoryQueue:
+    def __init__(self, max_length: int, num_envs: int, device: str = "cuda"):
+        self.max_length = max_length
+        self.num_envs = num_envs
+        self.device = device
+        self.queue = queue.Queue(maxsize=max_length)
+        # 10シーケンス以内にリセットされたかどうかを管理するテンソル
+        self.reseted_queue = queue.Queue(maxsize=max_length)
+
+    def append_state(self, state: torch.Tensor):
+        if self.queue.full():
+            self.queue.get()
+        self.queue.put(state)
+
+    def append_reseted(self, reseted: torch.Tensor):
+        if self.reseted_queue.full():
+            self.reseted_queue.get()
+        self.reseted_queue.put(reseted)
+
+    def get_state_sequence(self) -> torch.Tensor:
+        states = list(self.queue.queue)
+        return torch.stack(states, dim=0)  # shape: (sequence_length, num_envs, state_dim)
+
+    def ready(self) -> bool:
+        """
+        状態がGRUに入力可能かどうかを調べる。
+        これがFalseになるのは最初のmax_length個のステップだけ。
+        これがFalseの場合はGRUでの推論はスキップして、強制的に健康状態モデルで動かすことにする。
+        """
+        return self.queue.full()
+
+    def get_reseted_tensor(self) -> torch.Tensor:
+        """
+        直近sequence_length分のリセット情報から書く環境についてリセットがあったかどうかを示すmask tensorを返す。
+        マスクにはリセットがあった場合はTrueが入っている。
+        shape(num_envs,)
+        リセットがある場合は、その環境には十分なシーケンスが無いと判断するので、強制的に健康状態モデルを使うことになる。
+        """
+        if self.reseted_queue.qsize() < self.max_length:
+            # 十分な情報がない場合はリセットされている = 健康状態モデルを強制的に使うとする
+            return torch.ones((self.max_length,self.num_envs), dtype=torch.bool, device=self.device)
+        # 各環境について、直近max_length個のリセット情報を取得し、リセットがあった場合Trueにする shape: (num_envs, 1)
+        reseted_list = list(self.reseted_queue.queue)
+        reseted_tensor = torch.stack(reseted_list, dim=0).any(dim=0)
+        return reseted_tensor
+
+# fmt: off
+# [start-config-dict-torch]
+CUSTOM_TRAINER_DEFAULT_CONFIG = {
+    "timesteps": 100000,            # number of timesteps to train for
+    "headless": False,              # whether to use headless mode (no rendering)
+    "disable_progressbar": False,   # whether to disable the progressbar. If None, disable on non-TTY
+    "close_environment_at_exit": True,   # whether to close the environment on normal program termination
+    "environment_info": "episode",       # key used to get and log environment info
+    "stochastic_evaluation": False,      # whether to use actions rather than (deterministic) mean actions during evaluation
+}
+# [end-config-dict-torch]
+# fmt: on
+
+
+class CustomParallelAgentTrainer(Trainer):
+    def __init__(
+        self,
+        env: Wrapper,
+        health_agent: Agent,
+        agents: Union[Agent, List[Agent]],
+        agents_scope: Optional[List[int]] = None,
+        cfg: Optional[dict] = None,
+    ) -> None:
+        """Parallel agent trainer
+
+        Train agents sequentially (i.e., one after the other in each interaction with the environment)
+
+        :param env: Environment to train on
+        :type env: skrl.envs.wrappers.torch.Wrapper
+        :param agents: Agents to train
+        :type agents: Union[Agent, List[Agent]]
+        :param agents_scope: Number of environments for each agent to train on (default: ``None``)
+        :type agents_scope: tuple or list of int, optional
+        :param cfg: Configuration dictionary (default: ``None``).
+                    See CUSTOM_TRAINER_DEFAULT_CONFIG for default values
+        :type cfg: dict, optional
+        """
+        _cfg = copy.deepcopy(CUSTOM_TRAINER_DEFAULT_CONFIG)
+        _cfg.update(cfg if cfg is not None else {})
+        agents_scope = agents_scope if agents_scope is not None else []
+        super().__init__(env=env, agents=agents, agents_scope=agents_scope, cfg=_cfg)
+
+        # 健康状態で動かすためのエージェント。これはactしか使わない
+        self.health_agent = health_agent
+
+        # 状態履歴キューの初期化 これの長さはGRUのsequence_lengthに合わせる。なので10
+        self.state_history_queue = StateHistoryQueue(max_length=10,num_envs=self.num_envs, device=self.device)
+        
+        # JointGRUNetモデルの読み込み
+        # このモデルは出力を outputs > 0.6で二値化する必要があるので注意
+        self.joint_gru_net = JointGRUNet(input_size=69, hidden_size=128, output_size=19).to("cuda")
+        self.joint_gru_net.load_state_dict(torch.load("model.pth"))
+        self.joint_gru_net.eval()
+
+        # これは後でいらなくなったら消してね
+        if self.num_simultaneous_agents != 19:
+            raise ValueError("len(Agents) must be same as joint number.")
+
+        # init agents
+        if self.num_simultaneous_agents > 1:
+            for agent in self.agents:
+                agent.init(trainer_cfg=self.cfg)
+        else:
+            self.agents.init(trainer_cfg=self.cfg)
+
+    def train(self) -> None:
+        """Train the agents sequentially
+
+        This method executes the following steps in loop:
+
+        - Pre-interaction (sequentially)
+        - Compute actions (sequentially)
+        - Interact with the environments
+        - Render scene
+        - Record transitions (sequentially)
+        - Post-interaction (sequentially)
+        - Reset environments
+        """
+        # set running mode
+        if self.num_simultaneous_agents > 1:
+            for agent in self.agents:
+                agent.set_running_mode("train")
+        else:
+            self.agents.set_running_mode("train")
+
+        # non-simultaneous agents
+        if self.num_simultaneous_agents == 1:
+            # single-agent
+            if self.env.num_agents == 1:
+                self.single_agent_train()
+            # multi-agent
+            else:
+                self.multi_agent_train()
+            return
+
+        # reset env
+        states, infos = self.env.reset()
+
+        for timestep in tqdm.tqdm(
+            range(self.initial_timestep, self.timesteps), disable=self.disable_progressbar, file=sys.stdout
+        ):
+
+            # pre-interaction
+            # for agent in self.agents:
+                # これは今の所全く何もしない
+                # agent.pre_interaction(timestep=timestep, timesteps=self.timesteps)
+
+            # 状態履歴キューに現在の状態を追加
+            self.state_history_queue.append_state(states)
+
+            if not self.state_history_queue.ready():
+                # ここに入るのは最初のsequense_length個のステップだけ
+                with torch.no_grad():
+                    # health_agentはinitの中でevalになっていて、その後trainには切り替えないのでこのままで良い
+                    actions = self.health_agent.act(states, timestep=timestep, timesteps=self.timesteps)[0]
+                    next_states, _, terminated, truncated, _ = self.env.step(actions)
+                    # 面倒なのでログは飛ばす
+                    # 訓練も当然しない
+                    # reset environments
+                    if terminated.any() or truncated.any():
+                        with torch.no_grad():
+                            states, infos = self.env.reset()
+                    else:
+                        states = next_states
+                    reseted_tensor = terminated | truncated
+                    self.state_history_queue.append_reseted(reseted_tensor)
+                continue
+
+            with torch.no_grad():
+                # shape (num_envs, 19)
+                joint_failure_mask = self.joint_gru_net(self.state_history_queue.get_state_sequence())
+
+                # compute actions
+                actions = torch.vstack(
+                    [
+                        agent.act(states[scope[0] : scope[1]], timestep=timestep, timesteps=self.timesteps)[0]
+                        for agent, scope in zip(self.agents, self.agents_scope)
+                    ]
+                )
+
+                # step the environments
+                next_states, rewards, terminated, truncated, infos = self.env.step(actions)
+
+                # render scene
+                if not self.headless:
+                    self.env.render()
+
+                # record the environments' transitions
+                for agent, scope in zip(self.agents, self.agents_scope):
+                    agent.record_transition(
+                        states=states[scope[0] : scope[1]],
+                        actions=actions[scope[0] : scope[1]],
+                        rewards=rewards[scope[0] : scope[1]],
+                        next_states=next_states[scope[0] : scope[1]],
+                        terminated=terminated[scope[0] : scope[1]],
+                        truncated=truncated[scope[0] : scope[1]],
+                        infos=infos,
+                        timestep=timestep,
+                        timesteps=self.timesteps,
+                    )
+
+                # log environment info
+                if self.environment_info in infos:
+                    for k, v in infos[self.environment_info].items():
+                        if isinstance(v, torch.Tensor) and v.numel() == 1:
+                            for agent in self.agents:
+                                agent.track_data(f"Info / {k}", v.item())
+
+            # post-interaction
+            for agent in self.agents:
+                agent.post_interaction(timestep=timestep, timesteps=self.timesteps)
+
+            # reset environments
+            if terminated.any() or truncated.any():
+                with torch.no_grad():
+                    states, infos = self.env.reset()
+            else:
+                states = next_states
+            # リセット状態を状態履歴キューに追加
+            reseted_tensor = terminated | truncated
+            self.state_history_queue.append_reseted(reseted_tensor)
+
+    def eval(self) -> None:
+        """Evaluate the agents sequentially
+
+        This method executes the following steps in loop:
+
+        - Compute actions (sequentially)
+        - Interact with the environments
+        - Render scene
+        - Reset environments
+        """
+        # set running mode
+        if self.num_simultaneous_agents > 1:
+            for agent in self.agents:
+                agent.set_running_mode("eval")
+        else:
+            self.agents.set_running_mode("eval")
+
+        # non-simultaneous agents
+        if self.num_simultaneous_agents == 1:
+            # single-agent
+            if self.env.num_agents == 1:
+                self.single_agent_eval()
+            # multi-agent
+            else:
+                self.multi_agent_eval()
+            return
+
+        # reset env
+        states, infos = self.env.reset()
+
+        for timestep in tqdm.tqdm(
+            range(self.initial_timestep, self.timesteps), disable=self.disable_progressbar, file=sys.stdout
+        ):
+
+            # pre-interaction
+            for agent in self.agents:
+                agent.pre_interaction(timestep=timestep, timesteps=self.timesteps)
+
+            with torch.no_grad():
+                # compute actions
+                outputs = [
+                    agent.act(states[scope[0] : scope[1]], timestep=timestep, timesteps=self.timesteps)
+                    for agent, scope in zip(self.agents, self.agents_scope)
+                ]
+                actions = torch.vstack(
+                    [
+                        output[0] if self.stochastic_evaluation else output[-1].get("mean_actions", output[0])
+                        for output in outputs
+                    ]
+                )
+
+                # step the environments
+                next_states, rewards, terminated, truncated, infos = self.env.step(actions)
+
+                # render scene
+                if not self.headless:
+                    self.env.render()
+
+                # write data to TensorBoard
+                for agent, scope in zip(self.agents, self.agents_scope):
+                    agent.record_transition(
+                        states=states[scope[0] : scope[1]],
+                        actions=actions[scope[0] : scope[1]],
+                        rewards=rewards[scope[0] : scope[1]],
+                        next_states=next_states[scope[0] : scope[1]],
+                        terminated=terminated[scope[0] : scope[1]],
+                        truncated=truncated[scope[0] : scope[1]],
+                        infos=infos,
+                        timestep=timestep,
+                        timesteps=self.timesteps,
+                    )
+
+                # log environment info
+                if self.environment_info in infos:
+                    for k, v in infos[self.environment_info].items():
+                        if isinstance(v, torch.Tensor) and v.numel() == 1:
+                            for agent in self.agents:
+                                agent.track_data(f"Info / {k}", v.item())
+
+            # post-interaction
+            for agent in self.agents:
+                super(type(agent), agent).post_interaction(timestep=timestep, timesteps=self.timesteps)
+
+            # reset environments
+            if terminated.any() or truncated.any():
+                with torch.no_grad():
+                    states, infos = self.env.reset()
+            else:
+                states = next_states
---
+++ humanoidprac/humanoidprac/scripts/skrl/discriminator/libskrl/failure_model_train.py
@@ -0,0 +1,272 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Script to train RL agent with skrl.
+
+Visit the skrl documentation (https://skrl.readthedocs.io) to see the examples structured in
+a more user-friendly way.
+"""
+
+"""Launch Isaac Sim Simulator first."""
+
+import argparse
+import sys
+import ast
+
+from isaaclab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Train an RL agent with skrl.")
+parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
+parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
+parser.add_argument("--video_interval", type=int, default=2000, help="Interval between video recordings (in steps).")
+parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
+parser.add_argument("--task", type=str, default=None, help="Name of the task.")
+parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
+parser.add_argument(
+    "--distributed", action="store_true", default=False, help="Run training with multiple GPUs or nodes."
+)
+parser.add_argument("--checkpoint", type=str, default=None, help="Path to model checkpoint to resume training.")
+parser.add_argument("--max_iterations", type=int, default=None, help="RL Policy training iterations.")
+parser.add_argument(
+    "--ml_framework",
+    type=str,
+    default="torch",
+    choices=["torch", "jax", "jax-numpy"],
+    help="The ML framework used for training the skrl agent.",
+)
+parser.add_argument(
+    "--algorithm",
+    type=str,
+    default="PPO",
+    choices=["AMP", "PPO", "IPPO", "MAPPO"],
+    help="The RL algorithm used for training the skrl agent.",
+)
+parser.add_argument("--joint_cfg",type=str,default="",help="上書きするjointの設定を辞書形式で指定する. 例: '{\"left_knee_joint\": 150}'")
+
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli, hydra_args = parser.parse_known_args()
+# always enable cameras to record video
+if args_cli.video:
+    args_cli.enable_cameras = True
+
+# clear out sys.argv for Hydra
+sys.argv = [sys.argv[0]] + hydra_args
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import gymnasium as gym
+import os
+import random
+from datetime import datetime
+
+import skrl
+from packaging import version
+
+# check for minimum supported skrl version
+SKRL_VERSION = "1.4.2"
+if version.parse(skrl.__version__) < version.parse(SKRL_VERSION):
+    skrl.logger.error(
+        f"Unsupported skrl version: {skrl.__version__}. "
+        f"Install supported version using 'pip install skrl>={SKRL_VERSION}'"
+    )
+    exit()
+
+if args_cli.ml_framework.startswith("torch"):
+    from skrl.utils.runner.torch import Runner
+elif args_cli.ml_framework.startswith("jax"):
+    from skrl.utils.runner.jax import Runner
+
+from isaaclab.envs import (
+    DirectMARLEnv,
+    DirectMARLEnvCfg,
+    DirectRLEnvCfg,
+    ManagerBasedRLEnvCfg,
+    multi_agent_to_single_agent,
+)
+from isaaclab.utils.assets import retrieve_file_path
+from isaaclab.utils.dict import print_dict
+from isaaclab.utils.io import dump_pickle, dump_yaml
+
+from isaaclab_rl.skrl import SkrlVecEnvWrapper
+
+import isaaclab_tasks  # noqa: F401
+from isaaclab_tasks.utils.hydra import hydra_task_config
+
+import humanoidprac.tasks  # noqa: F401
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import logger  # noqa: F401
+
+# config shortcuts
+algorithm = args_cli.algorithm.lower()
+agent_cfg_entry_point = "skrl_cfg_entry_point" if algorithm in ["ppo"] else f"skrl_{algorithm}_cfg_entry_point"
+
+
+@hydra_task_config(args_cli.task, agent_cfg_entry_point)
+def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agent_cfg: dict):
+    """Train with skrl agent."""
+    # override configurations with non-hydra CLI arguments
+    env_cfg.scene.num_envs = args_cli.num_envs if args_cli.num_envs is not None else env_cfg.scene.num_envs
+    env_cfg.sim.device = args_cli.device if args_cli.device is not None else env_cfg.sim.device
+
+    # jointの設定を上書きする
+    if args_cli.joint_cfg:
+        param = ast.literal_eval(args_cli.joint_cfg)
+        print(f"[INFO] Overriding joint configuration : {env_cfg.scene.robot.actuators['legs'].effort_limit}")
+        for joint_name, torque in param.items():
+            env_cfg.scene.robot.actuators["legs"].effort_limit[joint_name] = torque
+            print(f"[INFO] Overriding joint configuration with: {joint_name} -> {torque}Nm")
+
+    # multi-gpu training config
+    if args_cli.distributed:
+        env_cfg.sim.device = f"cuda:{app_launcher.local_rank}"
+    # max iterations for training
+    if args_cli.max_iterations:
+        agent_cfg["trainer"]["timesteps"] = args_cli.max_iterations * agent_cfg["agent"]["rollouts"]
+    agent_cfg["trainer"]["close_environment_at_exit"] = False
+    # configure the ML framework into the global skrl variable
+    if args_cli.ml_framework.startswith("jax"):
+        skrl.config.jax.backend = "jax" if args_cli.ml_framework == "jax" else "numpy"
+
+    # randomly sample a seed if seed = -1
+    if args_cli.seed == -1:
+        args_cli.seed = random.randint(0, 10000)
+
+    # set the agent and environment seed from command line
+    # note: certain randomization occur in the environment initialization so we set the seed here
+    agent_cfg["seed"] = args_cli.seed if args_cli.seed is not None else agent_cfg["seed"]
+    env_cfg.seed = agent_cfg["seed"]
+
+    # specify directory for logging experiments
+    log_root_path = os.path.join("logs", "skrl", agent_cfg["agent"]["experiment"]["directory"])
+    log_root_path = os.path.abspath(log_root_path)
+    print(f"[INFO] Logging experiment in directory: {log_root_path}")
+    # specify directory for logging runs: {time-stamp}_{run_name}
+    log_dir = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + f"_{algorithm}_{args_cli.ml_framework}"
+    print(f"Exact experiment name requested from command line {log_dir}")
+    if agent_cfg["agent"]["experiment"]["experiment_name"]:
+        log_dir += f'_{agent_cfg["agent"]["experiment"]["experiment_name"]}'
+    # set directory into agent config
+    agent_cfg["agent"]["experiment"]["directory"] = log_root_path
+    agent_cfg["agent"]["experiment"]["experiment_name"] = log_dir
+    # update log_dir
+    log_dir = os.path.join(log_root_path, log_dir)
+
+    # dump the configuration into log-directory
+    dump_yaml(os.path.join(log_dir, "params", "env.yaml"), env_cfg)
+    dump_yaml(os.path.join(log_dir, "params", "agent.yaml"), agent_cfg)
+    dump_pickle(os.path.join(log_dir, "params", "env.pkl"), env_cfg)
+    dump_pickle(os.path.join(log_dir, "params", "agent.pkl"), agent_cfg)
+    joint_cfg_logger = logger.SettingLogger(env_cfg)
+    joint_cfg_logger.log_setting(os.path.join(log_dir, "joint_cfg.json"))
+
+    # get checkpoint path (to resume training)
+    resume_path = retrieve_file_path(args_cli.checkpoint) if args_cli.checkpoint else None
+
+    # create isaac environment
+    env = gym.make(args_cli.task, cfg=env_cfg, render_mode="rgb_array" if args_cli.video else None)
+
+    # convert to single-agent instance if required by the RL algorithm
+    if isinstance(env.unwrapped, DirectMARLEnv) and algorithm in ["ppo"]:
+        env = multi_agent_to_single_agent(env)
+
+    # wrap for video recording
+    if args_cli.video:
+        video_kwargs = {
+            "video_folder": os.path.join(log_dir, "videos", "train"),
+            "step_trigger": lambda step: step % args_cli.video_interval == 0,
+            "video_length": args_cli.video_length,
+            "disable_logger": True,
+        }
+        print("[INFO] Recording videos during training.")
+        print_dict(video_kwargs, nesting=4)
+        env = gym.wrappers.RecordVideo(env, **video_kwargs)
+
+    # wrap around environment for skrl
+    env = SkrlVecEnvWrapper(env, ml_framework=args_cli.ml_framework)  # same as: `wrap_env(env, wrapper="auto")`
+
+    # 学習済みエージェントの読み込み
+    learned_models = ["normal_agent.pt","knee_agent.pt"]
+    import yaml
+    learned_model_conf = yaml.safe_load(open("learned_agent_cfg.yaml"))
+    learned_agents = []
+    for model in learned_models:
+        rn = Runner(env, learned_model_conf)
+        rn.agent.load(model)
+        learned_agents.append(rn.agent)
+
+    from skrl.memories.torch import RandomMemory
+    memory = RandomMemory(
+        memory_size=agent_cfg["memory"]["memory_size"],
+        num_envs=env.num_envs,
+        device=env.device,
+    )
+
+    from skrl.utils.model_instantiators.torch import deterministic_model
+    import discrete_model
+    from gymnasium.spaces import Discrete
+
+    models = {}
+    # models["policy"] = discrete_model.MLP(
+    #                 observation_space=env.observation_space,
+    #                 action_space=Discrete(2),  # 2つのモデルから選ぶ行動
+    #                 device=env.device,
+    #             )
+    models["policy"] = discrete_model.GRU(
+                observation_space=env.observation_space,
+                action_space=Discrete(2),  # 2つのモデルから選ぶ行動
+                device=env.device,
+                num_envs=env.num_envs,
+                sequence_length=6,  # 6ステップ分の履歴を考慮。
+                # これは、ロールアウトの時のステップ数と関連がある。rollout=24,mini_batches=4であれば、
+                # 6ステップ分の履歴まで考慮できる。6ステップの、それの約数だけが使える。
+                )
+    models["value"] = deterministic_model(
+                    observation_space=env.observation_space,
+                    action_space=Discrete(2),  # discriminator用の離散アクション空間
+                    device=env.device,
+                    **agent_cfg["models"]["value"],
+                )
+
+    from skrl.resources.schedulers.torch import KLAdaptiveRL
+    agent_cfg["agent"]["learning_rate_scheduler"] = KLAdaptiveRL
+
+    # from skrl.agents.torch.ppo import PPO
+    from skrl.agents.torch.ppo import PPO_RNN as PPO
+    agent = PPO(
+        models=models,  # models dict
+        memory=memory,  # memory instance, or None if not required
+        cfg=agent_cfg["agent"],  # configuration dict (preprocessors, learning rate schedulers, etc.)
+        observation_space=env.observation_space,
+        action_space=Discrete(2),
+        device=env.device,
+    )
+
+    # load checkpoint (if specified)
+    if resume_path:
+        print(f"[INFO] Loading model checkpoint from: {resume_path}")
+        agent.load(resume_path)
+
+    # trainer = SequentialTrainer(env=env, agents=[agent], cfg=agent_cfg["trainer"])
+    from custom_trainer import CustomTrainer
+    custom_trainer = CustomTrainer(learned_agents=learned_agents,env=env,agents=[agent],cfg=agent_cfg["trainer"])
+    custom_trainer.train()
+
+    # close the simulator
+    env.close()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
